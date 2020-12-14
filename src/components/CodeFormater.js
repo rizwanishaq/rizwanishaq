11dc0037b33382e7f7431f9a2068c446950196a0
Rizwan Ishaq <rizwanishaq@gmail.com>
2020-12-14 01:20:02 +0100

    code highlighting and more pages

---
+++ src/components/CodeFormater.js
@@ -0,0 +1,11 @@
+import React, {useEffect} from 'react'
+
+const CodeFormater = ({code, language}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CodeFormater
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import Prism from "prismjs"
+import "prismjs/themes/prism-tomorrow.css";
 
 const HomePage = () => {
-    return (
-        <div>
-          Home  
-        </div>
-    )
+  useEffect(() => {
+    Prism.highlightAll();
+  },[])
+  return (
+    <div className="Code">
+      
+      <pre>
+      <code className='language-py'>
+     {
+     `
+     import tensorflow as tf
+     import numpy as np
+     ` 
+     }
+    </code>
+      
+        
+      </pre>
+    </div>
+  )
 }
 
 export default HomePage
